b6142bbe732e8ce384058171243318cd6a567569
Camilla Teodoro <camillateodoro@hotmail.com>
2023-04-19 11:54:24 -0300

    redux for beginners tutorial

--- starter/src/components/TodoItem.js
+++ starter/src/components/TodoItem.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleCompleteAsync, deleteTodo } from "../redux/todoSlice";
 
 const TodoItem = ({ id, title, completed }) => {
-	return (
-		<li className={`list-group-item ${completed && 'list-group-item-success'}`}>
-			<div className='d-flex justify-content-between'>
-				<span className='d-flex align-items-center'>
-					<input type='checkbox' className='mr-3' checked={completed}></input>
-					{title}
-				</span>
-				<button className='btn btn-danger'>Delete</button>
-			</div>
-		</li>
-	);
+  const dispatch = useDispatch();
+
+  const handleCheckboxClick = () => {
+    dispatch(toggleCompleteAsync({ id, completed: !completed }));
+  };
+
+  const handleDeleteClick = () => {
+    dispatch(deleteTodo({ id }));
+  };
+
+  return (
+    <li className={`list-group-item ${completed && "list-group-item-success"}`}>
+      <div className="d-flex justify-content-between">
+        <span className="d-flex align-items-center">
+          <input
+            type="checkbox"
+            className="mr-3"
+            onClick={handleCheckboxClick}
+            checked={completed}
+          ></input>
+          {title}
+        </span>
+        <button className="btn btn-danger" onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default TodoItem;
